5c3d56878fd936260fe581f77ac4ab7e3f93b2c1
rydkey <remyrisser@hotmail.fr>
2017-03-13 08:43:14 +0100

    création du assets/Maps/js/maps.js

---
+++ web/assets/Maps/js/maps.js
@@ -0,0 +1,32 @@
+/**
+ * Created by rydkey on 13/03/17.
+ */
+var geocoder;
+var map;
+src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCCjQF0jXVa3CpSscQuUMAwn_LbcGGdfDM&callback=initMap">
+
+function initialize() {
+    geocoder = new google.maps.Geocoder();
+    var latlng = new google.maps.LatLng(-34.397, 150.644);
+    var mapOptions = {
+        zoom: 15,
+        center: latlng,
+        scrollwheel: false
+    }
+    map = new google.maps.Map(document.getElementById("map"), mapOptions);
+}
+
+function codeAddress() {
+    var address = document.getElementById("address").value;
+    geocoder.geocode( { 'address': address}, function(results, status) {
+        if (status == google.maps.GeocoderStatus.OK) {
+            map.setCenter(results[0].geometry.location);
+            var marker = new google.maps.Marker({
+                map: map,
+                position: results[0].geometry.location
+            });
+        } else {
+            alert("Geocode was not successful for the following reason: " + status);
+        }
+    });
+}
